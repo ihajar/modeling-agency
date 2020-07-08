174deadf1a1709159deb8554ccf4ce6fb75b9bb6
ihajar <hadjero.hamaidi@gmail.com>
2020-07-08 22:12:05 +0100

    adding gsap animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,47 +5,137 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import React, {useRef, useEffect} from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
+import {TweenMax, TimelineMax, Power3} from 'gsap';
+import "../style/main.scss"
 
-import Header from "./header"
-import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+document.addEventListener("DOMContentLoaded", function(event) {
 
+  window.addEventListener("load", function(e) {
+    document.body.style.display = "block"
+    
+    let tl = new TimelineMax()
+
+    
+
+  
+      tl.staggerFrom('section', 2, {
+        opacity: 0,
+        scale: .5,
+        ease: Power3.easeOut
+      }, 0.2)
+
+      tl.staggerFrom('h1, h2', .5, {
+        opacity: 0,
+        y: -40,
+        ease: Power3.easeInOut
+      }, 0.2, "-=2")
+
+      tl.staggerFrom('.anim-panel', 1, {
+        opacity: 0,
+        y: -40,
+        ease: Power3.easeInOut
+      }, 0.2, "-=1.5")
+
+    })
+  }, false);
+
+
+
+function Layout() {
+  
+  let tl = new TimelineMax()
+
+  useEffect(() => {
+    // sections variables
+    // const section1 = setions.firstElementChild;
+    // const section2 = sections.lastElementChild;
+
+
+    TweenMax.to(app, 0, {css: {visibility: 'visible'}})
+    tl.staggerFrom('section', 2, {
+      opacity: 0,
+      scale: .5,
+      ease: Power3.easeOut
+    }, 0.2)
+
+    tl.staggerFrom('h1, h2', .5, {
+      opacity: 0,
+      y: -40,
+      ease: Power3.easeInOut
+    }, 0.2, "-=2")
+
+    tl.staggerFrom('.anim-panel', 1, {
+      opacity: 0,
+      y: -40,
+      ease: Power3.easeInOut
+    }, 0.2, "-=1.5")
+
+  })
+ 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <body>
+      <header>
+        <a href="" class="logo">CatWalk</a>
+        <nav>
+          <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div className="wrapper">
+        <section className="hero">
+          <div className="inner">
+            <div className="clipper">
+              <h1>Be discovered</h1>
+            </div>
+
+            <div className="anim-panel">
+              <p className="subtitle">By the world's largest brands in the model industry. </p>
+              <a href="#" className="read-btn primary">Register Now</a>
+            </div>
+          </div>      
+        </section>
+
+        <section className="sidebar">
+          <div className="inner">
+            <div className="clipper">
+              <h2>About</h2>
+            </div>
+            <div className="anim-panel">
+              <p>infor about the modeling agency</p>
+              <a href="" class="read-btn">Reach out</a>
+            </div>
+          </div>
+        </section>
+
+        <section className="subscribe">
+        <div className="inner">
+          <div className="clipper">
+            <h2>Subscribe</h2>
+          </div>
+          <div className="anim-panel">
+            <p>subscribe to our newsletter of our latest digital fashion shows.</p>
+            <input  type="text" name="email" placeholder="Your Email ..." />
+            <button className="cta">Subscribe</button>
+          </div>
+        </div>
+
+      </section>
       </div>
-    </>
+   
+    </body>
   )
 }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+
 
 
 const IndexPage = () => (
   <Layout>
-    
    
   </Layout>
 )
